300ef423b3925812b62275b09ffa7b988327a192
Rob Flynn <rob@thingerly.com>
2020-04-25 00:02:10 -0400

    Setup basic vue structure

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,30 @@
+<script>
+import Patd from './lib/patd'
+
+import Scene from './components/scene'
+import PlayerInput from './components/player_input'
+import Console from './components/console'
+
+export default {
+	name: 'App',
+	components: {
+		Scene,
+		Console,
+		PlayerInput
+	},
+	mounted() {
+		console.log("booted")
+	}
+}
+</script>
+
+<style lang="scss">
+</style>
+
 <template>
 	<div>
-		This is the app
+		<Scene />
+		<Console />
+		<PlayerInput />
 	</div>
 </template>
---
+++ src/components/player_input.vue
@@ -0,0 +1,8 @@
+<style lang="scss">
+</style>
+
+<template>
+  <div>
+    <input type="text" name="playerInput" ref="playerInput" />
+  </div>
+</template>
