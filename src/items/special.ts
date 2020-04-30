1fc53a3812129e824f9b564acb83096d6b64ab51
Rob Flynn <rob@thingerly.com>
2020-04-29 22:37:46 -0400

    Custom objects can now render intents.
    I don't like the way intents are currently registered, though.
    
    Should the registering item be the only one capable of responding
    to that intent?  Maybe?  Probably.

--- src/items/special.ts
+++ src/items/special.ts
@@ -1,14 +1,57 @@
+const Event = require("@/lib/events")
+import Patd from "@/lib/patd"
 import Item from "@/lib/item"
 import { Trait } from "@/lib/item"
+import Intent from '@/lib/intent'
 
 export default class SpecialThing extends Item {
+  timesTurned: number = 0
+
+  get description(): string {
+    let description = 'This thing is fairly special, I think. '
+
+    if (this.timesTurned == 0) {
+      description += 'I wonder what happens if you turn it?'
+    } else {
+      description += `It has been turned ${this.timesTurned} times.`
+    }
+
+    return description
+  }
+
   constructor() {
     super('special thing')
 
     this.id = 'special'
 
-    this.description = 'This thing is fairly special, I think.'
     this.environmental = 'Some kind of special thing is being special over there.'
     this.traits = [Trait.gettable]
+
+    // TODO: Make this prettier
+    this.registerIntent(Intent.createIntent(
+      ['turn the special thing'],
+      [],
+      () => {
+        this.emit(Event.actionResponse, 'You turn the special thing.')
+
+        if (this.timesTurned == 0) {
+         this.emit(Event.actionResponse, 'You hear a whirring in the distance.')
+        }
+
+        if (this.timesTurned == 1) {
+          this.emit(Event.actionResponse, 'The ground begins to trumble beneath your feet.')
+        }
+
+        if (this.timesTurned == 2) {
+          this.emit(Event.actionResponse, 'Drink more slurm.')
+        }
+
+        if (this.timesTurned == 43) {
+          this.emit(Event.actionResponse, `I can't believe you've done this.`)
+        }
+
+        this.timesTurned++
+      }
+    ))
   }
 }
--- src/lib/intent.ts
+++ src/lib/intent.ts
@@ -3,8 +3,28 @@ import GameObject from './game_object'
 import Item from './item'
 
 export default class Intent extends GameObject {
-  get verbs(): string[] { return [] }
-  get prepositions(): string[] { return [] }
+  get verbs(): string[] { return this._verbs }
+  get prepositions(): string[] { return this._prepositions }
 
-  perform(item?: Item, preposition?: string, target?: Item) {}
+  private _verbs: string[] = []
+  private _prepositions: string[] = []
+  private _perform?: Function
+
+  constructor(verbs?: string[], prepositions?: string[], perform?: Function) {
+    super()
+
+    if (verbs) { this._verbs = verbs }
+    if (prepositions) { this._prepositions = prepositions }
+    if (perform) { this._perform = perform }
+  }
+
+  perform(item?: Item, preposition?: string, target?: Item) {
+    if (this._perform) {
+      this._perform(item, preposition, target)
+    }
+  }
+
+  static createIntent(verbs: string[], prepositions: string[], perform: Function): Intent {
+    return new Intent(verbs, prepositions, perform)
+  }
 }
